acfefbe6591362b56728860a44126790f8acd888
Tina <tinalns15@gmail.com>
2022-12-09 14:06:24 -0500

    finish task 8

---
+++ src/main/java/com/example/itemsAPI/ItemsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.itemsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itemsAPI/ItemsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
